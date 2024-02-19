140c0fa0174b7bdc51fd8e5c5d63f50c7b5231df
Branko Bajcetic <bajcetic.branko@gmail.com>
2024-02-18 16:22:29 -0800

    Complete a basic vector implementation to get the vector test cases passing in gtest (7 tests were previously created but all failing, now all are passing)

--- vector/vector.cpp
+++ vector/vector.cpp
@@ -1,28 +1,55 @@
 #include "vector.h"
 #include <iostream>
-template <typename T>
-vector<T>::vector() {}
 
 /* Constructors */
 template <typename T>
-vector<T>::vector(int capacity) {}
+vector<T>::vector(): size_(0), capacity_(DEFAULT_CAPACITY) {
+    arr = new T[capacity_];
+}
+
+template <typename T>
+vector<T>::vector(int capacity): size_(0), capacity_(capacity) {
+    arr = new T[capacity_];
+}
+
 
 /* Destructor */
 template <typename T>
 vector<T>::~vector() {}
 
+
 /* Methods */
 template <typename T>
-void vector<T>::push_back(T item) {}
+void vector<T>::push_back(T item) {
+    if (size_ == capacity_) {
+        capacity_ *= 2;
+        T* newArr = new T[capacity_];
+        for (int i = 0; i < size_; i++) {
+            newArr[i] = arr[i];
+        }
+        delete[] arr;
+        arr = newArr;
+    }
+    arr[size_++] = item;
+}
 
 template <typename T>
-void vector<T>::pop_back() {}
+void vector<T>::pop_back() {
+    if (size_ == 0 || capacity_ == 0) { return; }
+    size_--;
+}
 
 template <typename T>
-void vector<T>::clear() {}
+void vector<T>::clear() {
+    delete[] arr;
+    size_ = 0;
+    capacity_ = DEFAULT_CAPACITY;
+    arr = new T[capacity_];
+}
 
 template <typename T>
 void vector<T>::print() const {}
+
 
 /* Operators */
 template <typename T>
